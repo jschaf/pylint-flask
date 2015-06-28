a8202e87cb91ea1c9e74233867a70edf2ce17266
Joe Schafer <joe@jschaf.com>
2015-06-28 02:32:16 -0400

    fix: first working stub

--- pylint_flask/__init__.py
+++ pylint_flask/__init__.py
@@ -14,18 +14,27 @@ def register(_):
     pass
 
 
-def flask_transform(_):
+def flask_transform(node):
     '''Replace Flask module with the imports it lazily loads.'''
-    pass
-    # module = AstroidBuilder().string_build(code)
-    # module.name = 'flask'
-    # return module
+    new_node = nodes.Import()
+    new_node.names = [('flask_wtf', 'wtf')]
+    new_node.lineno = node.lineno
+    new_node.fromlineno = node.fromlineno
+    new_node.tolineno = node.tolineno
+    new_node.col_offset = node.col_offset
+    new_node.parent = node.parent
 
+    return new_node
+
+def flask_transform_long_mod_name():
+    pass
+    # from flask.ext.wtf import Form
+    # from flask_wtf import Form
 
 def is_flask_module(node):
     '''Predicate for checking if we have the flask module.'''
-    return node.name == 'flask'
+    return node.modname == 'flask.ext'
 
-MANAGER.register_transform(nodes.Module,
+MANAGER.register_transform(nodes.From,
                            flask_transform,
                            is_flask_module)
