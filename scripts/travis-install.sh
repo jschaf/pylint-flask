c5221d8dc7bf39a8d167087b29858cd13a391d7a
Joe Schafer <joe@jschaf.com>
2015-06-28 17:27:41 -0400

    refactor(build): move all build requirements into dev-requirements.txt

--- scripts/travis-install.sh
+++ scripts/travis-install.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
-pip install coverage coveralls
 pip install -r dev-requirements.txt
-pip install git+https://github.com/landscapeio/pylint-plugin-utils.git@develop
 pip install --editable .
